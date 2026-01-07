71e7cb592cbe162a0467f712907778be0c953520
devolexa <starovatovasasha@gmail.com>
2026-01-07 14:36:49 +0200

    some hero styles

--- src/js/mobile.menu.js
+++ src/js/mobile.menu.js
@@ -1,16 +1,19 @@
 export default function mobileMenu() {
     const open = document.querySelector('[data-menu-open]');
     const close = document.querySelector('[data-menu-close]');
-    const menu = document.querySelector('[data-menu]')
+    const menu = document.querySelector('[data-menu]');
+    const header = document.querySelector('#header');
 
     if (!menu) return;
 
     open?.addEventListener('click', () => {
         menu.classList.add('is-open');
         document.body.style.overflow = 'hidden';
+        header.style.position = 'static';
     });
     close?.addEventListener('click', () => {
         menu.classList.remove('is-open');
         document.body.style.overflow = '';
+        header.style.position = 'fixed';
     }); 
 } 
