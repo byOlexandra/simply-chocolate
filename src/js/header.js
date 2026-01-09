0599331634a3f20a90370894d8758d2935090e40
devolexa <starovatovasasha@gmail.com>
2026-01-09 15:44:00 +0200

    sync toggles

--- src/js/header.js
+++ src/js/header.js
@@ -1,22 +1,32 @@
 export function toggleTheme() {
-    const toggleThemeBtn = document.getElementById('theme-toggle');
+    const toggleThemeBtn = document.querySelectorAll('.theme-toggle-input');
     const currentTheme = localStorage.getItem('theme');
+    const syncToggles = (isDark) => {
+        toggleThemeBtn.forEach(el => {
+            el.checked = isDark;
+        })
+    }
     if (currentTheme) {
         document.body.setAttribute('data-theme', currentTheme);
         if (currentTheme === 'dark') {
             toggleThemeBtn.checked = true;
+            syncToggles(true);
         } else {
             toggleThemeBtn.checked = false;
         }
     }
-    toggleThemeBtn.addEventListener('change', () => {
-        if (toggleThemeBtn.checked) {
-            document.body.setAttribute('data-theme', 'dark');
-            localStorage.setItem('theme', 'dark');
-        } else {
-            document.body.setAttribute('data-theme', 'light');
-            localStorage.setItem('theme', 'light');
-        }        
+    toggleThemeBtn.forEach(btn => {
+        btn.addEventListener('change', () => {
+            const isDark = btn.checked;
+            if(btn.checked) {
+                document.body.setAttribute('data-theme', 'dark');
+                localStorage.setItem('theme', 'dark');
+            } else {
+                document.body.setAttribute('data-theme', 'light');
+                localStorage.setItem('theme', 'light');
+            }
+            syncToggles(isDark);
+        })
     })
 }
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import './main.scss'
 import mobileMenu from './js/mobile.menu';
-mobileMenu();
 import { toggleTheme, initHeaderScroll } from './js/header';
-toggleTheme();
-initHeaderScroll()
+
+document.addEventListener('DOMContentLoaded', () => {
+    mobileMenu();
+    toggleTheme();
+    initHeaderScroll();
+})
