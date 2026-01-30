3f5b8d2fc8a651cdbadf3af9e5ea8ad115f1e8c4
devolexa <starovatovasasha@gmail.com>
2026-01-30 18:30:00 +0200

    Update feedback section

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -1,4 +1,9 @@
 import { reviews } from './data/reviewsData';
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 const STORAGE_KEY = 'reviews_data';
 const url = 'https://randomuser.me/api/?results=10';
@@ -6,21 +11,55 @@ const feedbackList = document.querySelector('.feedback__list');
 
 function reviewTemplate(obj) {
   return `
-        <li class='feedback__list-item'>
+        <div class='swiper-slide'>
+        <div class='feedback__list-item'>
             <img src="${obj.photo}" alt="picture of a person who left a review"/>
             <h3 class="feedback__username">${obj.name}</h3>
             <p class="feedback__text">${obj.text}</p>
-        </li>
+        </div>
+        </div>
+        
     `;
 }
 
 function renderReviews(arr) {
   if (!feedbackList) return;
   feedbackList.innerHTML = arr.map(reviewTemplate).join('');
+
+  initSwiper();
+}
+
+function initSwiper() {
+    new Swiper('.swiper', {
+      modules: [Navigation, Pagination],
+      loop: true,
+        slidesPerView: 1,
+      observer: true,
+        observeParents: true,
+    centeredSlides: false, // Не центрувати слайд, щоб не бачити сусідні
+  watchSlidesProgress: true,
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    breakpoints: {
+      768: { slidesPerView: 2 },
+      1440: { slidesPerView: 3 },
+    },
+  });
 }
 
 async function fetchUsers() {
-  const cachedData = localStorage.getItem(STORAGE_KEY);
+  const cachedData = sessionStorage.getItem(STORAGE_KEY);
   if (cachedData) {
     renderReviews(JSON.parse(cachedData));
     return;
@@ -38,7 +77,7 @@ async function fetchUsers() {
       name: `${users[index].name.first} ${users[index].name.last}`,
       photo: `${users[index].picture.large}`,
     }));
-      localStorage.setItem(STORAGE_KEY, JSON.stringify(combinedReviews));
+    sessionStorage.setItem(STORAGE_KEY, JSON.stringify(combinedReviews));
     renderReviews(combinedReviews);
   } catch (error) {
     console.error('Fetch error:', error);
@@ -48,3 +87,5 @@ async function fetchUsers() {
 export default function initReviews() {
   fetchUsers();
 }
+
+
