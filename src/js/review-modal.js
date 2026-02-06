3dd64786106f3a5e3df285d4ac2d782bbb80dfbc
Староватова Олександра <ist251_sov@student.ztu.edu.ua>
2026-02-06 13:33:07 +0200

    Set styles for button in review-modal

--- src/js/review-modal.js
+++ src/js/review-modal.js
@@ -1,2 +1,24 @@
-const backdrop = document.querySelector('.review-backdrop');
+export function initReviewModal() {
+    const backdrop = document.querySelector('.review-backdrop');
+    const openModalBtn = document.querySelector('#leaveReviewBtn');
+    const closeModalBtn = document.querySelector('#closeModal')
+
+    openModalBtn.addEventListener('click', e => {
+        e.preventDefault()
+        backdrop.classList.add('is-open');
+        document.body.style.overflow = 'hidden'
+    })
+
+    closeModalBtn.addEventListener('click', () => {
+        backdrop.classList.remove('is-open');
+        document.body.style.overflow = 'visible'
+    })
+
+    backdrop.addEventListener('click', (e) => {
+        if (e.target === e.currentTarget) {
+            backdrop.classList.remove('is-open');
+            document.body.style.overflow = 'visible'
+        }
+    })
+}
 
--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import './main.scss'
 import mobileMenu from './js/mobile.menu';
 import { toggleTheme, initHeaderScroll } from './js/header';
 import initReviews from './js/feedback';
+import { initReviewModal } from './js/review-modal';
 
 document.addEventListener('DOMContentLoaded', () => {
     mobileMenu();
     toggleTheme();
     initHeaderScroll();
-    initReviews()
+    initReviews();
+    initReviewModal();
 })
