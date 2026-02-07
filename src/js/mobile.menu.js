09123038b4260ac1eef312b62709dbb175753d27
Староватова Олександра <ist251_sov@student.ztu.edu.ua>
2026-02-07 16:32:32 +0200

    Refactor mobile-menu.js

--- src/js/mobile.menu.js
+++ src/js/mobile.menu.js
@@ -1,18 +1,31 @@
+const menu = document.querySelector('[data-menu]');
+const open = document.querySelector('[data-menu-open]');
+const close = document.querySelector('[data-menu-close]');
+const header = document.querySelector('#header');
+ 
 export default function mobileMenu() {
-    const open = document.querySelector('[data-menu-open]');
-    const close = document.querySelector('[data-menu-close]');
-    const menu = document.querySelector('[data-menu]');
-    const header = document.querySelector('#header');
+  if (!menu) return;
 
-    if (!menu) return;
+    close.addEventListener('click', closeMenu)
+    open.addEventListener('click', openMenu)
 
-    open?.addEventListener('click', () => {
-        menu.classList.add('is-open');
-        document.body.style.overflow = 'hidden';
-    });
-    close?.addEventListener('click', () => {
-        menu.classList.remove('is-open');
-        document.body.style.overflow = '';
-        header.style.position = 'fixed';
-    }); 
-} 
+    menu.addEventListener('click', (e) => {
+    if (e.target.closest('.mm__list-a-item')) {
+      closeMenu();
+    }
+  });
+}
+
+const closeMenu = () => {
+ if (!menu) return;
+  menu.classList.remove('is-open');
+  document.body.style.overflow = '';
+  if (header) header.style.position = 'fixed';
+};
+
+const openMenu = () => {
+   if (!menu) return; 
+    menu.classList.add('is-open');
+    document.body.style.overflow = 'hidden';
+}
+
