5766fec8689a69ee628205a434fc37fd33a37512
Староватова Олександра <ist251_sov@student.ztu.edu.ua>
2026-02-10 21:57:26 +0200

    Set up form logic

--- src/js/order-modal.js
+++ src/js/order-modal.js
@@ -1,3 +1,8 @@
+import { Notyf } from 'notyf';
+import 'notyf/notyf.min.css';
+import * as yup from 'yup';
+import IMask from 'imask';
+
 const openOrder = document.querySelector('#open-order-modal');
 const backdrop = document.querySelector('.order-backdrop');
 const closeBtn = document.querySelector('#closeOrderModal');
@@ -6,6 +11,51 @@ const selectedChocInput = document.getElementById('selectedChocInput');
 const orderForm = document.getElementById('orderForm');
 const deliverySelect = document.getElementById('deliveryMethod');
 const dynamicContainer = document.getElementById('dynamic-fields-container');
+const phoneInput = document.getElementById('userPhone');
+
+const deliveryWays = ['nova_poshta', 'courier', 'pickup'];
+
+const schema = yup.object().shape({
+  customer_name: yup
+    .string()
+    .required('Your name is required')
+    .min(2, 'At least 2 letters'),
+  customer_phone: yup
+    .string()
+    .required('Enter your phone')
+    .test('len', 'Invalid phone number', val => {
+      const clean = val.replace(/\D/g, '');
+      return clean.length === 12;
+    }),
+  customer_email: yup.string().required('You email is required').email(),
+  delivery_method: yup
+    .string()
+    .oneOf(deliveryWays)
+    .required('Choose a delivery method'),
+  customer_npoffice: yup
+    .number()
+    .transform((value, originalValue) =>
+      originalValue === '' ? undefined : value
+    )
+    .when('delivery_method', {
+      is: 'nova_poshta',
+      then: schema => schema.required('Enter an office number'),
+      otherwise: schema => schema.notRequired(),
+    }),
+  customer_address: yup.string().when('delivery_method', {
+    is: 'courier',
+    then: schema => schema.required('Enter address'),
+    otherwise: schema => schema.notRequired(),
+  }),
+  customer_city: yup.string().required('Enter a city'),
+  customer_comment: yup.string().max(250, 'Must be less than 250 letters'),
+  is_gift: yup.boolean(),
+});
+
+const clearErrorMessages = () =>
+  document
+    .querySelectorAll('.review-modal__error-message')
+    .forEach(el => (el.textContent = ''));
 
 const toggleModal = (backdrop, isOpen) => {
   backdrop.classList.toggle('is-open', isOpen);
@@ -40,7 +90,7 @@ function renderModalOptions(chocolates) {
     )
     .join('');
   setupSelectionLogic();
-  setupDeliveryLogic()
+  setupDeliveryLogic();
 }
 
 function setupSelectionLogic() {
@@ -63,35 +113,84 @@ function setupSelectionLogic() {
 }
 
 function setupDeliveryLogic() {
-  deliverySelect.addEventListener('change', (e) => {
-  const method = e.target.value;
-  
-  dynamicContainer.innerHTML = '';
+  deliverySelect.addEventListener('change', e => {
+    const method = e.target.value;
 
-  if (method === 'nova_poshta') {
-    dynamicContainer.innerHTML = `
+    dynamicContainer.innerHTML = '';
+
+    if (method === 'nova_poshta') {
+      dynamicContainer.innerHTML = `
       <div class="order__form-group">
         <label class="order__label" for="npOffice">Post Office Number</label>
-        <input class="order__input" type="text" id="npOffice" name="np_office" placeholder="e.g. 1545" />
+        <input class="order__input" type="text" id="npOffice" name="customer_npoffice" placeholder="e.g. 1545" />
+        <span class="order__error-message" id="error-customer_npoffice"></span>
       </div>
     `;
-  } else {
-    dynamicContainer.innerHTML = `
+    } else {
+      dynamicContainer.innerHTML = `
       <div class="order__form-group">
             <label class="order__label" for="userAddress">Address</label>
             <input class="order__input" type="text" id="userAddress" name="customer_address"
                 placeholder="e.g. Ukrainska street" />
+                <span class="order__error-message" id="error-customer_address"></span>
         </div>
     `;
+    }
+  });
+}
+
+const maskOptions = {
+  mask: '+{38}(000)000-00-00',
+  lazy: false,
+};
+const mask = IMask(phoneInput, maskOptions);
+
+function handleSubmission() {
+  if (orderForm) {
+    orderForm.addEventListener('submit', async e => {
+      e.preventDefault();
+      if (!selectedChocInput.value) {
+        notyf.error('Please choose a chocolate first');
+        return
+      }
+
+      clearErrorMessages();
+
+      const formData = new FormData(orderForm);
+      const orderDetails = Object.fromEntries(formData.entries());
+
+      orderDetails.customer_phone = mask.value;
+      orderDetails.is_gift = orderForm.elements.is_gift.checked;
+
+      try {
+        await schema.validate(orderDetails, { abortEarly: false });
+        notyf.success(
+          `Thank you, ${orderDetails.customer_name}! Your order for ${orderDetails.selected_chocolate} has been received.`
+        );
+        orderForm.reset();
+        mask.value = '';
+        mask.updateValue();
+      } catch (error) {
+        if (error.inner) {
+          error.inner.forEach(err => {
+            const errSpan = document.querySelector(`#error-${err.path}`);
+            if (errSpan) errSpan.textContent = err.message;
+          });
+        }
+      }
+    });
   }
-});
 }
 
 export function initOrderModal() {
   if (openOrder && backdrop) {
     openOrder.addEventListener('click', () => {
-      console.log('Кнопку відкриття натиснуто!');
       toggleModal(backdrop, true);
+      orderForm.reset();
+      mask.value = '';
+      mask.updateValue();
+
+      clearErrorMessages();
     });
   }
 
@@ -109,26 +208,22 @@ export function initOrderModal() {
       toggleModal(backdrop, false);
     }
   });
-
-  if (orderForm) {
-    orderForm.addEventListener('submit', e => {
-    e.preventDefault();
-
-    if (!selectedChocInput.value) {
-      alert('Please select a chocolate first!');
-      return;
-    }
-
-    const formData = new FormData(orderForm);
-    const orderDetails = Object.fromEntries(formData.entries());
-
-    console.log('Order Sent:', orderDetails);
-
-    alert(
-      `Thank you, ${orderDetails.customer_name}! Your order for ${orderDetails.selected_chocolate} has been received.`
-    );
-  });
-  }
-
-  
+  handleSubmission();
 }
+
+const notyf = new Notyf({
+  duration: 2000,
+  position: { x: 'center', y: 'top' },
+  types: [
+    {
+      type: 'success',
+      background: '#FD9222',
+      duration: 4000,
+    },
+    {
+      type: 'error',
+      background: '#ff0000',
+      duration: 4000,
+    },
+  ],
+});
--- src/js/review-modal.js
+++ src/js/review-modal.js
@@ -4,65 +4,66 @@ import { Notyf } from 'notyf';
 import 'notyf/notyf.min.css';
 
 const notyf = new Notyf({
-    duration: 2000,
-    position: { x: 'center', y: 'top' },
-    types: [
-      {
-        type: 'success',
-        background: '#FD9222',
-        duration: 4000,
-      },
-    ],
-  });
+  duration: 2000,
+  position: { x: 'center', y: 'top' },
+  types: [
+    {
+      type: 'success',
+      background: '#FD9222',
+      duration: 4000,
+    },
+  ],
+});
+
+
 
 const clearErrorMessages = () =>
   document
     .querySelectorAll('.review-modal__error-message')
     .forEach(el => (el.textContent = ''));
 
-
 const schema = yup.object().shape({
-username: yup
+  username: yup
     .string()
     .required('Your name is required')
     .min(2, 'At least 2 letters'),
-email: yup.string().required('You email is required').email(),
-phone: yup
+  email: yup.string().required('You email is required').email(),
+  phone: yup
     .string()
     .required('Enter your phone')
     .test('len', 'Invalid phone number', val => {
-    const clean = val.replace(/\D/g, '');
-    return clean.length === 12;
+      const clean = val.replace(/\D/g, '');
+      return clean.length === 12;
     }),
-comment: yup
+  comment: yup
     .string()
     .max(250, 'Must be less than 250 letters')
     .required('Enter your thoughts'),
-terms: yup
+  terms: yup
     .boolean()
     .oneOf([true], 'You must agree with terms')
     .required('You must agree with terms'),
 });
 
 const toggleModal = (backdrop, isOpen) => {
-    backdrop.classList.toggle('is-open', isOpen);
-    document.body.style.overflow = isOpen ? 'hidden' : 'visible';
-}
+  backdrop.classList.toggle('is-open', isOpen);
+  document.body.style.overflow = isOpen ? 'hidden' : 'visible';
+};
 
 export function initReviewModal() {
   const backdrop = document.querySelector('.review-backdrop');
   const openModalBtn = document.querySelector('#leaveReviewBtn');
-    const closeModalBtn = document.querySelector('#closeModal');
-    const form = document.querySelector('.review-modal__form');
-    const phoneInput = document.getElementById('phone');
-    
-    if (!form || !phoneInput || !backdrop) return;
+  const closeModalBtn = document.querySelector('#closeModal');
+  const form = document.querySelector('.review-modal__form');
+  const phoneInput = document.getElementById('phone');
 
-    const maskOptions = {
+  if (!form || !phoneInput || !backdrop) return;
+
+  const maskOptions = {
     mask: '+{38}(000)000-00-00',
     lazy: false,
-    };
-    const mask = IMask(phoneInput, maskOptions);
+  };
+  const mask = IMask(phoneInput, maskOptions);
 
   openModalBtn.addEventListener('click', () => {
     backdrop.classList.add('is-open');
@@ -72,27 +73,27 @@ export function initReviewModal() {
     mask.value = '';
     mask.updateValue();
 
-      clearErrorMessages();
-      toggleModal(backdrop, true)
+    clearErrorMessages();
+    toggleModal(backdrop, true);
   });
 
   closeModalBtn.addEventListener('click', () => {
-      toggleModal(backdrop, false);
+    toggleModal(backdrop, false);
   });
 
   backdrop.addEventListener('click', e => {
     if (e.target === e.currentTarget) {
-        toggleModal(backdrop, false)
+      toggleModal(backdrop, false);
     }
   });
 
   document.addEventListener('keydown', e => {
     if (e.code === 'Escape' && backdrop.classList.contains('is-open')) {
-      toggleModal(backdrop, false)
+      toggleModal(backdrop, false);
     }
   });
 
-    form.addEventListener('submit', async e => {      
+  form.addEventListener('submit', async e => {
     e.preventDefault();
     clearErrorMessages();
 
