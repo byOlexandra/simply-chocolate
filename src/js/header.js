7851c552dcde67a76e36423261b0abe6109a2600
Староватова Олександра <ist251_sov@student.ztu.edu.ua>
2026-03-05 12:00:25 +0200

    Improve toggle theme logic

--- src/js/header.js
+++ src/js/header.js
@@ -1,50 +1,60 @@
 const header = document.getElementById('header');
+const theme = document.getElementById('theme-toggle');
 
 export function toggleTheme() {
-    const toggleThemeBtn = document.querySelectorAll('.theme-toggle-input');
-    const currentTheme = localStorage.getItem('theme');
-    const syncToggles = (isDark) => {
-        toggleThemeBtn.forEach(el => {
-            el.checked = isDark;
-        })
+  const toggleThemeBtn = document.querySelectorAll('.theme-toggle-input');
+  const currentTheme = localStorage.getItem('theme');
+  const syncToggles = isDark => {
+    toggleThemeBtn.forEach(el => {
+      el.checked = isDark;
+    });
+  };
+
+  if (currentTheme) {
+    document.body.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+      toggleThemeBtn.checked = true;
+      syncToggles(true);
+    } else {
+      toggleThemeBtn.checked = false;
     }
-    if (currentTheme) {
-        document.body.setAttribute('data-theme', currentTheme);
-        if (currentTheme === 'dark') {
-            toggleThemeBtn.checked = true;
-            syncToggles(true);
-        } else {
-            toggleThemeBtn.checked = false;
-        }
-    }
-    toggleThemeBtn.forEach(btn => {
-        btn.addEventListener('change', () => {
-            const isDark = btn.checked;
-            if(btn.checked) {
-                document.body.setAttribute('data-theme', 'dark');
-                localStorage.setItem('theme', 'dark');
-            } else {
-                document.body.setAttribute('data-theme', 'light');
-                localStorage.setItem('theme', 'light');
-            }
-            syncToggles(isDark);
-        })
-    })
+  }
+
+  toggleThemeBtn.forEach(btn => {
+    btn.addEventListener('change', () => {
+      const isDark = btn.checked;
+
+      if (btn.checked) {
+        document.body.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+      } else {
+        document.body.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+      }
+
+      syncToggles(isDark);
+    });
+  });
 }
 
+theme.addEventListener('keydown', e => {
+  if (e.code === 'Enter') {
+    theme.click();
+  }
+});
+
 export function initHeaderScroll() {
-    window.addEventListener('scroll', checkScroll);
+  window.addEventListener('scroll', checkScroll);
 
-    window.addEventListener('DOMContentLoaded', checkScroll);
+  window.addEventListener('DOMContentLoaded', checkScroll);
 
-    window.addEventListener('load', checkScroll);
+  window.addEventListener('load', checkScroll);
 }
 
 function checkScroll() {
-    if (window.scrollY > 0) {
-        header.classList.add('header-scrolled'); 
-    } else {
-        header.classList.remove('header-scrolled');
-    }
+  if (window.scrollY > 0) {
+    header.classList.add('header-scrolled');
+  } else {
+    header.classList.remove('header-scrolled');
+  }
 }
-
