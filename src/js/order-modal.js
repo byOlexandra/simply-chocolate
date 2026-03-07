78780e5c82b133d4a1216c582087f8cb6eec9192
Староватова Олександра <ist251_sov@student.ztu.edu.ua>
2026-03-07 17:07:14 +0200

    feat: implement localstorage in order modal

--- src/js/order-modal.js
+++ src/js/order-modal.js
@@ -3,8 +3,8 @@ import 'notyf/notyf.min.css';
 import * as yup from 'yup';
 import IMask from 'imask';
 import { closeMenu } from './mobile-menu';
-import TomSelect from "tom-select";
-import "tom-select/dist/css/tom-select.css";
+import TomSelect from 'tom-select';
+import 'tom-select/dist/css/tom-select.css';
 
 const openOrder = document.querySelector('#open-order-modal');
 const backdrop = document.querySelector('.order-backdrop');
@@ -15,64 +15,68 @@ const orderForm = document.getElementById('orderForm');
 const deliverySelect = document.getElementById('deliveryMethod');
 const dynamicContainer = document.getElementById('dynamic-fields-container');
 const phoneInput = document.getElementById('userPhone');
-const basketIcons = document.querySelectorAll('[data-open-order]')
+const basketIcons = document.querySelectorAll('[data-open-order]');
+// const form = document.querySelector('.order-modal__form');
+
+const STORAGE_KEY = 'order-form-state';
 
 const deliveryWays = ['nova_poshta', 'courier', 'pickup'];
 
-const schema = yup.object().shape({
-  customer_name: yup
-    .string()
-    .required('Your name is required')
-    .min(2, 'At least 2 letters'),
-  customer_phone: yup
-    .string()
-    .required('Enter your phone')
-    .test('len', 'Invalid phone number', val => {
-      const clean = val.replace(/\D/g, '');
-      return clean.length === 12;
-    }),
-  customer_email: yup.string().required('You email is required').email(),
-  delivery_method: yup
-    .string()
-    .oneOf(deliveryWays)
-    .required('Choose a delivery method'),
-  customer_npoffice: yup
-    .number()
-    .transform((value, originalValue) =>
-      originalValue === '' ? undefined : value
-    )
-    .when('delivery_method', {
-      is: 'nova_poshta',
-      then: schema => schema.required('Enter an office number'),
-      otherwise: schema => schema.notRequired(),
-    }),
-  customer_address: yup.string().when('delivery_method', {
-    is: 'courier',
-    then: schema => schema.required('Enter address'),
-    otherwise: schema => schema.notRequired(),
-  }),
-  customer_city: yup.string().required('Enter a city'),
-  customer_comment: yup.string().max(250, 'Must be less than 250 letters'),
-  is_gift: yup.boolean(),
+function initOrderModal() {
+  const openActions = () => {
+    toggleModal(backdrop, true);
+    mask.value = '';
+    initFormValues()
+    clearErrorMessages();
+  }
+
+  if (basketIcons) {
+    basketIcons.forEach(icon => {
+      icon.addEventListener('click', openActions);
+    });
+  }
+  if (openOrder && backdrop) {
+    openOrder.addEventListener('click', openActions);
+  }
+
+  closeBtn.addEventListener('click', () => {
+    toggleModal(backdrop, false);
+  });
+
+  backdrop.addEventListener('click', e => {
+    if (e.target === e.currentTarget) {
+      toggleModal(backdrop, false);
+    }
+  });
+
+  document.addEventListener('keydown', e => {
+    if (e.code === 'Escape' && backdrop.classList.contains('is-open')) {
+      toggleModal(backdrop, false);
+    }
+  });
+
+  initFormValues();
+  handleSubmission();
+}
+
+orderForm.addEventListener('input', e => {
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+  const { name, value, type, checked } = e.target;
+  if (!name) return;
+  if (name === 'customer_phone') {
+  savedData[name] = mask.value; 
+  } else if (type === 'checkbox') {
+    savedData[name] = checked;
+  } else {
+    savedData[name] = value;
+  }
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(savedData));
 });
 
-const clearErrorMessages = () =>
-  document
-    .querySelectorAll('.review-modal__error-message')
-    .forEach(el => (el.textContent = ''));
-
-const toggleModal = (backdrop, isOpen) => {
-  backdrop.classList.toggle('is-open', isOpen);
-  
-  if (isOpen) {
-    document.body.classList.add('no-scroll');
-  } else {
-    document.body.classList.remove('no-scroll');
-  }
-};
-
 export async function fetchChocolateData() {
-  initOrderModal()
+  initOrderModal();
   try {
     const res = await fetch('./chocolates.json');
     if (!res.ok) {
@@ -122,43 +126,88 @@ function setupSelectionLogic() {
   });
 }
 
-function setupDeliveryLogic() {
-    new TomSelect("#deliveryMethod", {
-      create: false,
-      placeholder: "Select method",
-      allowEmptyOption: true,
-    });
+orderForm.addEventListener('input', e => {
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
-  deliverySelect.addEventListener('change', e => {
-    const method = e.target.value;
+  savedData[e.target.name] = e.target.value;
 
-    dynamicContainer.innerHTML = '';
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(savedData));
+});
 
-    if (method === 'nova_poshta') {
-      dynamicContainer.innerHTML = `
-      <div class="order__form-group">
-        <label class="order__label" for="npOffice">Post Office Number</label>
-        <input class="order__input" type="text" id="npOffice" name="customer_npoffice" placeholder="e.g. 1545" autocomplete="off" />
-        <span class="order__error-message" id="error-customer_npoffice"></span>
-      </div>
-    `;
-    } else {
-      dynamicContainer.innerHTML = `
-      <div class="order__form-group">
-            <label class="order__label" for="userAddress">Address</label>
-            <input class="order__input" type="text" id="userAddress" name="customer_address" autocomplete="street-address"
-                placeholder="e.g. Ukrainska street" />
-                <span class="order__error-message" id="error-customer_address"></span>
-        </div>
-    `;
+function initFormValues() {
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (!savedData) return;
+
+  Object.keys(savedData).forEach((key) => {
+    const field = orderForm.querySelector(`[name="${key}"]`);
+    
+    if (field) {
+      if (key === 'customer_phone') {
+        mask.value = savedData[key];
+        mask.updateValue();
+      } 
+      else if (field.type === 'checkbox') {
+        field.checked = savedData[key];
+      }
+      else {
+        field.value = savedData[key];
+      }
     }
   });
+}
+
+function setupDeliveryLogic() {
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  const initialMethod = savedData.delivery_method || '';
+
+  const selectLib = new TomSelect('#deliveryMethod', {
+    create: false,
+    placeholder: 'Select method',
+    controlInput: null,
+  });
+
+  if (initialMethod) {
+    selectLib.setValue(initialMethod);
+    renderDeliveryFields(initialMethod);
+  }
+
+  selectLib.on('change', value => {
+    const data = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+    data.delivery_method = value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+
+   renderDeliveryFields(value);
+  });
+}
+
+function renderDeliveryFields(method) {
+  dynamicContainer.innerHTML = '';
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+  if (method === 'nova_poshta') {
+    dynamicContainer.innerHTML = `
+      <div class="order__form-group">
+        <label class="order__label" for="npOffice">Post Office Number</label>
+        <input class="order__input" type="text" id="npOffice" name="customer_npoffice" 
+               value="${savedData.customer_npoffice || ''}" placeholder="e.g. 1545" autocomplete="off" />
+        <span class="order__error-message" id="error-customer_npoffice"></span>
+      </div>`;
+  } else if (method !== 'pickup' && method !== '') {
+    dynamicContainer.innerHTML = `
+      <div class="order__form-group">
+        <label class="order__label" for="userAddress">Address</label>
+        <input class="order__input" type="text" id="userAddress" name="customer_address" 
+               value="${savedData.customer_address || ''}" autocomplete="street-address" placeholder="e.g. Ukrainska street" />
+        <span class="order__error-message" id="error-customer_address"></span>
+      </div>`;
+  }
 }
 
 const maskOptions = {
   mask: '+{38}(000)000-00-00',
   lazy: false,
 };
+
 const mask = IMask(phoneInput, maskOptions);
 
 function handleSubmission() {
@@ -167,7 +216,7 @@ function handleSubmission() {
       e.preventDefault();
       if (!selectedChocInput.value) {
         notyf.error('Please choose a chocolate first');
-        return
+        return;
       }
 
       clearErrorMessages();
@@ -184,6 +233,7 @@ function handleSubmission() {
           `Thank you, ${orderDetails.customer_name}! Your order for ${orderDetails.selected_chocolate} has been received.`
         );
         orderForm.reset();
+        localStorage.removeItem(STORAGE_KEY);
         mask.value = '';
         mask.updateValue();
       } catch (error) {
@@ -196,51 +246,6 @@ function handleSubmission() {
       }
     });
   }
-}
-
-function initOrderModal() {
-  if (basketIcons) {
-    basketIcons.forEach(icon => {
-      icon.addEventListener('click', e => {
-        toggleModal(backdrop, true);
-        closeMenu()
-      orderForm.reset();
-      mask.value = '';
-      mask.updateValue();
-
-      clearErrorMessages();
-    })
-    })
-    
-  }
-  if (openOrder && backdrop) {
-    openOrder.addEventListener('click', () => {
-      toggleModal(backdrop, true);
-      orderForm.reset();
-      mask.value = '';
-      mask.updateValue();
-
-      clearErrorMessages();
-    });
-  }
-
-  closeBtn.addEventListener('click', () => {
-    toggleModal(backdrop, false);
-  });
-
-  backdrop.addEventListener('click', e => {
-    if (e.target === e.currentTarget) {
-      toggleModal(backdrop, false);
-    }
-  });
-
-  document.addEventListener('keydown', e => {
-    if (e.code === 'Escape' && backdrop.classList.contains('is-open')) {
-      toggleModal(backdrop, false);
-    }
-  });
-
-  handleSubmission();
 }
 
 const notyf = new Notyf({
@@ -259,3 +264,56 @@ const notyf = new Notyf({
     },
   ],
 });
+
+const schema = yup.object().shape({
+  customer_name: yup
+    .string()
+    .required('Your name is required')
+    .min(2, 'At least 2 letters'),
+  customer_phone: yup
+    .string()
+    .required('Enter your phone')
+    .test('len', 'Invalid phone number', val => {
+      const clean = val.replace(/\D/g, '');
+      return clean.length === 12;
+    }),
+  customer_email: yup.string().required('You email is required').email(),
+  delivery_method: yup
+    .string()
+    .oneOf(deliveryWays)
+    .required('Choose a delivery method'),
+  customer_npoffice: yup
+    .number()
+    .transform((value, originalValue) =>
+      originalValue === '' ? undefined : value
+    )
+    .when('delivery_method', {
+      is: 'nova_poshta',
+      then: schema => schema.required('Enter an office number'),
+      otherwise: schema => schema.notRequired(),
+    }),
+  customer_address: yup.string().when('delivery_method', {
+    is: 'courier',
+    then: schema => schema.required('Enter address'),
+    otherwise: schema => schema.notRequired(),
+  }),
+  customer_city: yup.string().required('Enter a city'),
+  customer_comment: yup.string().max(250, 'Must be less than 250 letters'),
+  is_gift: yup.boolean(),
+});
+
+function clearErrorMessages() {
+  document
+      .querySelectorAll('.review-modal__error-message')
+      .forEach(el => (el.textContent = ''));
+}
+
+function toggleModal(backdrop, isOpen) {
+    backdrop.classList.toggle('is-open', isOpen);
+
+  if (isOpen) {
+    document.body.classList.add('no-scroll');
+  } else {
+    document.body.classList.remove('no-scroll');
+  }
+}
